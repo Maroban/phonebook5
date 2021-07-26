cef7244c21111e4345098d7d3c59919af9459990
Maroban <dbtmdqja207@naver.com>
2021-07-26 19:20:09 +0900

    07/26 Dao, Controller

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -1,6 +1,8 @@
 package com.javaex.controller;
 
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.dao.PhoneDao;
 import com.javaex.vo.PersonVo;
@@ -37,7 +40,7 @@ public class PhoneController {
 		model.addAttribute("pList", personList);
 
 		// Front Controller ==> list.jsp 포워드
-		return "/WEB-INF/views/list.jsp";
+		return "list";
 	}
 
 	/*** 등록 폼 ***/
@@ -45,7 +48,8 @@ public class PhoneController {
 	public String writeForm() {
 		System.out.println("[PhoneController.writeForm]");
 
-		return "/WEB-INF/views/writeForm.jsp";
+		// Front Controller ==> wirteFOrm.jsp 포워드
+		return "writeForm";
 	}
 
 	/*** 등록 ***/
@@ -62,13 +66,25 @@ public class PhoneController {
 		return "redirect:/list";
 	}
 
+	/*** 등록 버전 2 ***/
+	@RequestMapping(value = "/write2", method = { RequestMethod.GET, RequestMethod.POST })
+	public String write2(@RequestParam("name") String name, @RequestParam("hp") String hp,
+			@RequestParam("company") String company) {
+
+		// insert2() 메소드 사용
+		phoneDao.insert2(name, hp, company);
+
+		// 리다이렉트
+		return "redirect:/list";
+	}
+
 	/*** 삭제 ***/
 	@RequestMapping(value = "/delete/{person_id}", method = { RequestMethod.GET, RequestMethod.POST })
 	public String delete(@PathVariable("person_id") int person_id) {
 		System.out.println("[PhoneController.delete]");
 
 		// delete() 메소드 사용
-		// phoneDao.delete(person_id);
+		phoneDao.delete(person_id);
 
 		// 리다이렉트
 		return "redirect:/list";
@@ -79,13 +95,27 @@ public class PhoneController {
 	public String modifyForm(@PathVariable("person_id") int person_id, Model model) {
 
 		// 한사람 정보 가져오기
-		// PersonVo onePerson = phoneDao.getPerson(person_id);
+		PersonVo personVo = phoneDao.getPerson(person_id);
 
 		// model에 담기
-		// model.addAttribute("onePerson", onePerson);
+		model.addAttribute("personVo", personVo);
 
 		// modifyForm.jsp 포워드
-		return "/WEB-INF/views/modifyForm.jsp";
+		return "modifyForm";
+	}
+
+	/*** 수정 폼 버전 2 ***/
+	@RequestMapping(value = "/modifyForm2/{person_id}", method = { RequestMethod.GET, RequestMethod.POST })
+	public String modifyForm2(@PathVariable("person_id") int person_id, Model model) {
+
+		// 한사람 정보 가져오기
+		Map<String, Object> personMap = phoneDao.getPerson2(person_id);
+
+		// model에 담기
+		model.addAttribute("pMap", personMap);
+
+		// modifyForm.jsp 포워드
+		return "modifyForm2";
 	}
 
 	/*** 수정 ***/
@@ -93,7 +123,7 @@ public class PhoneController {
 	public String modify(@ModelAttribute PersonVo personVo) {
 
 		// update() 메소드 사용
-		// phoneDao.update(personVo);
+		phoneDao.update(personVo);
 
 		// 리다이렉트
 		return "redirect:/list";
--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -1,6 +1,8 @@
 package com.javaex.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,48 @@ public class PhoneDao {
 	public void insert(PersonVo personVo) {
 
 		sqlSession.insert("phonebook.insert", personVo);
+	}
+
+	// 전화번호 등록 버전 2
+	public void insert2(String name, String hp, String company) {
+
+		Map<String, Object> personMap = new HashMap<String, Object>();
+		personMap.put("name", name);
+		personMap.put("hp", hp);
+		personMap.put("company", company);
+
+		sqlSession.insert("phonebook.insert2", personMap);
+	}
+
+	// 전화번호 삭제
+	public void delete(int person_no) {
+
+		sqlSession.delete("phonebook.delete", person_no);
+	}
+
+	// 전화번호 1명 출력
+	public PersonVo getPerson(int person_id) {
+
+		// 받아올 데이터(전화번호 1명)를 Vo로 묶는다.
+		PersonVo personVo = sqlSession.selectOne("phonebook.selectOne", person_id);
+
+		return personVo;
+	}
+
+	// 전화번호 1명 출력 버전 2
+	public Map<String, Object> getPerson2(int person_id) {
+
+		// 받아올 데이터(전화번호 1명)를 Map으로 묶는다.
+		Map<String, Object> personMap = sqlSession.selectOne("phonebook.selectOne2", person_id);
+
+
+		return personMap;
+	}
+
+	// 전화번호 수정
+	public void update(PersonVo personVo) {
+
+		sqlSession.update("phonebook.update", personVo);
 
 	}
 
